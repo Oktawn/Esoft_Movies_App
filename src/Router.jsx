288f2aac2b282f976f9f928cac4919058e80ddda
oktawn <belyhmitia@gmail.com>
2024-05-05 21:30:00 +0500

    spa

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,10 +4,6 @@ import MainLayout from "./layouts/MainLayout";
 import Main from "./pages/Main";
 import Search from "./pages/Search";
 import Films from "./pages/Films";
-import FilmsLayout from "./layouts/FilmsLayout";
-
-const getFilmPath = (path) => `/films${path}`;
-
 
 function Router() {
   return (
@@ -16,12 +12,8 @@ function Router() {
         <Route path="/" element={<MainLayout />}>
           <Route path="/" element={<Main />} />
           <Route path="/Search" element={<Search />} />
+          <Route path="/films/:id" element={<Films />} />
         </Route>
-
-        <Route path="/films" element={<FilmsLayout />}>
-          <Route element={<Films />} path={getFilmPath("/:id")} />
-        </Route>
-
         <Route element={<PageNotFound />} path="*" />
       </Routes>
     </BrowserRouter>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,11 @@
+import { useShallow } from "zustand/react/shallow";
 import CardList from "../components/CardList/CardList";
 import Navigate from "../components/Navigate/Navigate";
 import { useCard } from "../components/store/store";
 import { useState } from "react";
 
 function Main() {
-  const [films, setFilms] = useState(useCard((state) => state.cards));
+  const [films, setFilms] = useState(useCard(useShallow((state) => state.cards)));
   return (
     <div className="container">
       <Navigate films={films} state={setFilms} />
